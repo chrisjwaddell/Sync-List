4cae1ef87d57fa29a8c85986a7537f028d3ec4a9
Chris Waddell <yesitschris@hotmail.com>
2021-06-15 01:38:24 +1000

    Bugs fixed

--- app.js
+++ app.js
@@ -77,85 +77,19 @@ app.put('/test2', async function(req, res) {
 
 app.put('/build', async function(req, res) {
   console.log("=====================================================================");
-  // console.log(typeof(req.body))
   let json;
 
   try {
     json = await services.putBuild(req.body)
     // console.log("app.js after putBuild")
-    // console.log(typeof json)
   } catch(err) {
     // console.error("putBuild error")
-    // console.error(err)
-    // json = {}
+    console.error(err)
+    json = {}
   }
-
-  // console.log("in app.js - /build - json")
-
-  // let s = JSON.stringify(json)
-  // let s = await JSON.stringify(json)
-  // console.log(typeof json)
-  // console.log(s)
-  // console.log(typeof s)
-
-  // console.log(req.body)
-  // res.send(req.body)
-  // res.send(json)
-  // res.json(req.body)
-  // res.json(json)
-  // console.log(s)
-  // res.send(json)
-
-  // res.json(json)
-
-  // console.log(typeof req.body)
-  // console.log(typeof json)
 
   res.json(req.body);
 })
-
-app.put('/testjsonjson', async function(req, res) {
-  let json = req.body
-  // console.log("req.body - ")
-  // console.log(req.body)
-
-  await fsp.writeFile("E:\\wamp64\\www\\Websites-I-Did\\Sync-Listaaa\\Backup-scripts\\test.ps1", "test").then( r => {
-    debugger
-    console.log("f")
-    console.log("result")
-    res.json(json)
-    // return result;
-  })
-  .catch(err => console.log("ERROR"))
-
-})
-
-
-app.put('/testjsontext', async function(req, res, next) {
-  let json = req.body
-  console.log("req.body - ")
-  console.log(req.body)
-
-  res.send("Hello")
-})
-
-
-app.put('/test', async function(req, res, next) {
-  console.log("=====================================================================")
-  console.log(typeof(req.body))
-  let json
-
-  try {
-    // json = await services.test(req.body)
-    json = await services.putBuild(req.body)
-
-  } catch(err) {
-
-}
-
-  res.json(req.body)
-})
-
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
